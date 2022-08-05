5406e1138902ce9fc07dded9581c1d60101e1439
Dmitriy Demchenko <iam@ddemchenko.ru>
2022-08-05 13:14:18 +0300

    modify_popup

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -5,48 +5,40 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button-close');
 let popup = document.querySelector('.popup');
+let titleText = document.querySelector('.profile__title');
+let subtitleText = document.querySelector('.profile__subtitle');
+let inputName = document.querySelector('input[id=name]');
+let inputAboutme = document.querySelector('input[id=aboutme]');
 
-function showPopup () {
-  popup.removeAttribute('class', 'popup_opened');
+function showPopup() {
+  popup.classList.add('popup_opened');
+  inputName.setAttribute('value', titleText.innerText);
+  inputAboutme.setAttribute('value', subtitleText.innerText);
 }
 
-function hidePopup () {
-  popup.setAttribute('class', 'popup_opened');
-  inputName.removeAttribute('value', titleText);
-  inputAboutme.removeAttribute('value', subtitleText);
+function hidePopup() {
+  popup.classList.remove('popup_opened');
+  inputName.removeAttribute('value');
+  inputAboutme.removeAttribute('value');
 
 }
 
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', hidePopup);
 
-//inputs
-
-
-let titleText = document.querySelector('.profile__title').innerText;
-let subtitleText = document.querySelector('.profile__subtitle').innerText;
-let inputName = document.querySelector('input[id=name]');
-let inputAboutme = document.querySelector('input[id=aboutme]');
-
-inputName.setAttribute('value', titleText);
-inputAboutme.setAttribute('value', subtitleText);
-
 
 //form
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('input[id=name]');
-let jobInput = formElement.querySelector('input[id=aboutme]');
-let nameTitle = document.querySelector('.profile__title').value;
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameTitle.textContent = nameInput;
-    subtitleText.textContent = jobInput;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  titleText.innerText = (inputName).value;
+  subtitleText.innerText = (inputAboutme).value;
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
-console.log(nameTitle);
+formElement.addEventListener('submit', formSubmitHandler);
+console.log(nameTitle, nameSubtitle);
 
 
 
