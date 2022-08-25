030f2dab2acbb1c27c53d51e08b220730af487f4
Dmitriy Demchenko <iam@ddemchenko.ru>
2022-08-25 23:40:38 +0300

    feat: add new cards

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const closeButtonProfile = document.querySelector('.popup__button-close_section_
 const closeButtonElements = document.querySelector('.popup__button-close_section_elements');
 const addButton = document.querySelector('.profile__button');
 
+
 //Popup
 let popup_profile = document.querySelector('.popup_section_profile');
 let popup_elements = document.querySelector('.popup_section_elements');
@@ -13,50 +14,19 @@ let popup_elements = document.querySelector('.popup_section_elements');
 let titleText = document.querySelector('.profile__title');
 let subtitleText = document.querySelector('.profile__subtitle');
 
-//Input
-let inputName = document.querySelector('.popup__input_field_name');
-let inputAboutme = document.querySelector('.popup__input_field_aboutme');
-
 //Form
-let formElement = document.querySelector('.popup__form');
+let formProfile = document.querySelector('.popup__form_section_profile');
+const formElement = document.querySelector('.popup__form_section_elements');
+
+//Input
+let inputName = formProfile.querySelector('.popup__input_field_name');
+let inputAboutme = formProfile.querySelector('.popup__input_field_aboutme');
+const inputTitle = formElement.querySelector('.popup__input_field_title');
+const inputLink = formElement.querySelector('.popup__input_field_link');
 
 
-function hidePopup() {
-  popup_profile.classList.remove('popup_opened');
-}
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  titleText.textContent = inputName.value;
-  subtitleText.textContent = inputAboutme.value;
-  hidePopup();
-}
-
-//Edit button
-editButton.addEventListener('click', function() {
-  popup_profile.classList.add('popup_opened');
-  inputName.value = titleText.textContent;
-  inputAboutme.value = subtitleText.textContent;
-});
-//Close button
-closeButtonProfile.addEventListener('click', function() {
-  popup_profile.classList.remove('popup_opened');
-});
-
-closeButtonElements.addEventListener('click', function() {
-  popup_elements.classList.remove('popup_opened');
-});
-//Add button
-addButton.addEventListener('click', function() {
-  popup_elements.classList.add('popup_opened');
-});
-
-
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-//TemplateAddCards
-
+// Cards
 const initialCards = [
   {
     name: 'Архыз',
@@ -84,21 +54,71 @@ const initialCards = [
   }
 ]; 
 
-listElement = document.querySelector('.elements');
 
-initialCards.forEach(item =>  {
-   const card = `
-  <div class="card">
-  <img class="card__image" src="${item.link}" alt="${item.name}">
-  <div class="card__sign">
-    <h2 class="card__location">${item.name}</h2>
-    <button type="button" class="card__like"></button>
-  </div>
-</div> 
-  `
-  listElement.insertAdjacentHTML('afterbegin', card);
+
+//Popup hide
+function hidePopup() {
+  popup_profile.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  titleText.textContent = inputName.value;
+  subtitleText.textContent = inputAboutme.value;
+  hidePopup();
+}
+
+//Edit button
+editButton.addEventListener('click', function() {
+  popup_profile.classList.add('popup_opened');
+  inputName.value = titleText.textContent;
+  inputAboutme.value = subtitleText.textContent;
 });
 
+//Close button
+closeButtonProfile.addEventListener('click', function() {
+  popup_profile.classList.remove('popup_opened');
+});
+
+closeButtonElements.addEventListener('click', function() {
+  popup_elements.classList.remove('popup_opened');
+});
+
+//Add button
+addButton.addEventListener('click', function() {
+  popup_elements.classList.add('popup_opened');
+});
+
+function addCard (itemTitle, itemLink) {
+  const card = `
+ <div class="card">
+ <img class="card__image" src="${itemLink}" alt="${itemTitle}">
+ <div class="card__sign">
+   <h2 class="card__location">${itemTitle}</h2>
+   <button type="button" class="card__like"></button>
+ </div>
+</div> 
+ `
+ listElement.insertAdjacentHTML('afterbegin', card);
+};
+
+
+listElement = document.querySelector('.elements'); // Select section for add cards
+
+
+
+formElement.addEventListener('submit', (ev) => {
+ev.preventDefault();
+const fieldTitle = inputTitle.value;
+const fieldLink = inputLink.value; 
+addCard(fieldTitle, fieldLink);
+
+});
+
+initialCards.forEach((element) => {
+  addCard(element.name, element.link);
+})
+ // Function call
 
 
 
