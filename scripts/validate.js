334ab6889f51aeeca81af0ad21e3c3e201f72f44
Dmitriy Demchenko <iam@ddemchenko.ru>
2022-09-05 20:05:30 +0300

    js:validation_ready

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,6 +1,7 @@
 const formElement = document.querySelector('.popup__form');
 const inputElement = formElement.querySelector('.popup__input');
 
+
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input_type_error');
@@ -13,7 +14,7 @@ const hideInputError = (formElement, inputElement) => {
   inputElement.classList.remove('popup__input_type_error');
   errorElement.textContent = '';
   errorElement.classList.remove('popup__input-error_active')
-  
+
 };
 
 const isValid = (formElement, inputElement) => {
@@ -27,9 +28,13 @@ const isValid = (formElement, inputElement) => {
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button-submit');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+
     });
   });
 };
@@ -44,8 +49,26 @@ const enableValidation = () => {
   });
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__button-submit_disable');
+    buttonElement.disabled = true;
+    buttonElement.value = 'Disabled';
+  }
+  else {
+    buttonElement.classList.remove('popup__button-submit_disable');
+    buttonElement.disabled = false;
+    buttonElement.value = 'Enabled';
+  }
+};
+
 enableValidation();
 
 
 
-  
